1f81cd2ae77550ad9430b7c1284b01f83df1c919
HarleySG <harley.santos.garzon@gmail.com>
2018-12-23 21:54:15 -0500

    update: refactor component - include progress component

--- src/componets/Weather/index.js
+++ src/componets/Weather/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 import weatherDataFormat from "./../../services/queryWeatherAPI";
-import { iconsWeather } from "../../constants/const.weathersStates";
 import { apiWeatherURL } from "../../constants/const.weatherAPI";
 
 import Location from "./Location";
@@ -10,22 +10,16 @@ import "./weather.css";
 // https://www.udemy.com/share/100054AkAad1lUQHQ=/?xref=E0AYcFhVQ3kJSV82AT0GJVUWTx4dChQ%2BVFE=
 
 
-const currentDataComponent = {
-    temperature: 18,
-    weatherState: iconsWeather["cloud"],
-    humidity: 80,
-    wind: "10 m/s"
-}
-
 class WeatherLocation extends Component {
 
     constructor() {
         super();
-
+         
         this.state = {
             city: "Bogotá",
-            data: currentDataComponent
-        }
+            data: null
+        };
+
     }
 
     handleUpdatetClick = (e) => {
@@ -34,13 +28,22 @@ class WeatherLocation extends Component {
             .then(data => this.setState(weatherDataFormat(data)));
     }
 
+    componentDidMount() {
+        this.handleUpdatetClick(); 
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+    }
+
     render() {
         const { city, data } = this.state
         return (
             <div className="c-weather">
                 <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdatetClick}>update</button>
+                {data 
+                    ? <WeatherData data={data}></WeatherData>
+                    : <LinearProgress/>
+                }
             </div>
         )
     }

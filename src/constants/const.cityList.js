0e9dd2a9fe9446898f5c18eeba89c88e60096755
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2019-06-15 23:32:04 -0500

    new: file lista de ciudades para el api weather

---
+++ src/constants/const.cityList.js
@@ -0,0 +1,155 @@
+const cityList = [
+    {
+      "id": 5128638,
+      "name": "New York",
+      "country": "US",
+      "coord": {
+        "lon": -75.499901,
+        "lat": 43.000351
+      }
+    },{
+      "id": 4164138,
+      "name": "Miami",
+      "country": "US",
+      "coord": {
+        "lon": -80.193657,
+        "lat": 25.774269
+      }
+    },{
+      "id": 3169070,
+      "name": "Roma",
+      "country": "IT",
+      "coord": {
+        "lon": 12.4839,
+        "lat": 41.894741
+      }
+    },{
+      "id": 5855797,
+      "name": "Hawaii",
+      "country": "US",
+      "coord": {
+        "lon": -156.500275,
+        "lat": 20.75028
+      }
+    },{
+      "id": 3688689,
+      "name": "Bogota",
+      "country": "CO",
+      "coord": {
+        "lon": -74.081749,
+        "lat": 4.60971
+      }
+    },{
+      "id": 5815135,
+      "name": "Washington",
+      "country": "US",
+      "coord": {
+        "lon": -120.501472,
+        "lat": 47.500118
+      }
+    },{
+      "id": 3435910,
+      "name": "Buenos Aires",
+      "country": "AR",
+      "coord": {
+        "lon": -58.377232,
+        "lat": -34.613152
+      }
+    },{
+      "id": 3996063,
+      "name": "Mexico",
+      "country": "MX",
+      "coord": {
+        "lon": -102,
+        "lat": 23
+      }
+    },{
+      "id": 3674962,
+      "name": "Medellin",
+      "country": "CO",
+      "coord": {
+        "lon": -75.563591,
+        "lat": 6.25184
+      }
+    },{
+      "id": 3675707,
+      "name": "Madrid",
+      "country": "CO",
+      "coord": {
+        "lon": -74.264191,
+        "lat": 4.73244
+      }
+    },{
+      "id": 3646738,
+      "name": "Caracas",
+      "country": "VE",
+      "coord": {
+        "lon": -66.879189,
+        "lat": 10.48801
+      }
+    },{
+      "id": 3860259,
+      "name": "Cordoba",
+      "country": "AR",
+      "coord": {
+        "lon": -64.181053,
+        "lat": -31.4135
+      }
+    },{
+      "id": 6356055,
+      "name": "Barcelona",
+      "country": "ES",
+      "coord": {
+        "lon": 2.12804,
+        "lat": 41.399422
+      }
+    },{
+      "id": 6359304,
+      "name": "Madrid",
+      "country": "ES",
+      "coord": {
+        "lon": -3.68275,
+        "lat": 40.489349
+      }
+    },{
+      "id": 6325494,
+      "name": "Quebec",
+      "country": "CA",
+      "coord": {
+        "lon": -71.214539,
+        "lat": 46.812279
+      }
+    },{
+      "id": 6077243,
+      "name": "Montreal",
+      "country": "CA",
+      "coord": {
+        "lon": -73.587807,
+        "lat": 45.508839
+      }
+    },{
+      "id": 3941584,
+      "name": "Cusco",
+      "country": "PE",
+      "coord": {
+        "lon": -71.978058,
+        "lat": -13.51833
+      }
+    },
+];
+
+const cities = [
+    'new york,usa', 
+    'miami,usa',
+    "roma,it",
+    "hawaii,us",
+    "Bogota,co",
+    "Washington,us",
+    "Buenos aires,ar",
+    "mexico,mx",
+]
+
+module.exports = {
+    citiesID: cityList,
+    citiesName: cities,
+}

8e0cc861edade5c6a1bde6698f67ae4ed106165a
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2019-06-15 23:32:41 -0500

    update: ajuste link api weather

--- src/constants/const.weatherAPI.js
+++ src/constants/const.weatherAPI.js
@@ -1,3 +1,4 @@
-
-export const url_base = 'http://api.openweathermap.org/data/2.5/weather';
+const apiOpenweathermap = 'https://api.openweathermap.org/data/2.5/';
+export const url_weather = `${apiOpenweathermap}weather`;
+export const url_forecast = `${apiOpenweathermap}forecast`;
 export const api_key = '4a8bd0bc43e8116ca67b5e4ce64d84a9';

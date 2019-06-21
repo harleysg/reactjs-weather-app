c11724e724d705e4c6de652bf85896cb99fcb0ac
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2019-06-20 22:38:00 -0500

    new: service service.transformForcastQuery.js para filtrar información del response API

---
+++ src/services/service.transformForcastQuery.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+import weatherDataFormat from './queryWeatherAPI'
+
+const transformForcastQueryService = data => (
+    
+    data.list.filter(item => (
+            moment.unix(item.dt).utc().hour() === 6 ||
+            moment.unix(item.dt).utc().hour() === 12 ||
+            moment.unix(item.dt).utc().hour() === 18
+        )).map(item => {
+            const {data} = weatherDataFormat(item)
+            return (
+                {
+                    weekDay: moment.unix(item.dt).utc().format('ddd'),
+                    hour: moment.unix(item.dt).utc().hour(),
+                    data
+                }
+            )
+        }
+    )
+);
+
+export default transformForcastQueryService;

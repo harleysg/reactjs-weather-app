572e9f555538f97685819940325b1aff3b79f435
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2019-07-06 21:46:29 -0500

    update: #reFactor, logica de componente Weather se pasa del App.js -> compoennte/Weather/index.js, se inicia redux en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,71 +1,9 @@
-import React, { Component } from "react";
-/**@libraries */
-import { Grid } from "react-flexbox-grid";
-import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
+import React from "react";
 /** @styles */
 import "./App.css";
-/** @constants */
-import { citiesID } from "./constants/const.cityList";
 /** @componentes */
-import { WeatherList, ForeCastExtended } from "./components/Weather";
+import { Weather } from "./components/Weather";
 
-class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            city: null,
-            isOpenModal: false
-        };
-    }
-
-    handledModal = stateModal => {
-        if (stateModal === false) {
-            this.setState({ isOpenModal: false });
-        } else {
-            this.setState({ isOpenModal: true });
-        }
-    };
-
-    handleSelectionLocation = city => {
-        this.setState({
-            city,
-            isOpenModal: true
-        });
-    };
-
-    render() {
-        const { city } = this.state;
-        const { isOpenModal } = this.state;
-        return (
-            <div className="c-weather" data-open={isOpenModal}>
-                <AppBar position="sticky">
-                    <Grid>
-                        <Toolbar>
-                            <Typography variant="title" color="inherit">
-                                Weather App
-                            </Typography>
-                        </Toolbar>
-                    </Grid>
-                </AppBar>
-                <Grid>
-                    <WeatherList
-                        cities={citiesID}
-                        onSelectedLocation={this.handleSelectionLocation}
-                    />
-                    {city ? (
-                        <ForeCastExtended
-                            cityID={city.id}
-                            isOpen={isOpenModal}
-                            onHandledModal={this.handledModal}
-                        />
-                    ) : null}
-                </Grid>
-            </div>
-        );
-    }
-}
+const App = () => <Weather/>
 
 export default App;
--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,2 +1,73 @@
-export { default as WeatherList } from "./_list";
-export { default as ForeCastExtended } from "./_foreCastExtended";
+import React, { Component } from "react";
+/**@libraries */
+import { Grid } from "react-flexbox-grid";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import { createStore } from "redux";
+/** @constants */
+import { citiesID } from "constants/const.cityList";
+/** @componentes */
+import WeatherList from "./_list";
+import ForeCastExtended from "./_foreCastExtended";
+
+const store = createStore(() => {},
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export class Weather extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            city: null,
+            isOpenModal: false
+        };
+    }
+
+    handledModal = stateModal => {
+        if (stateModal === false) {
+            this.setState({ isOpenModal: false });
+        } else {
+            this.setState({ isOpenModal: true });
+        }
+    };
+
+    handleSelectionLocation = city => {
+        this.setState({
+            city,
+            isOpenModal: true
+        });
+        store.dispatch({ type: "SET_CITY", value: city });
+    };
+
+    render() {
+        const { city } = this.state;
+        const { isOpenModal } = this.state;
+        return (
+            <div className="c-weather" data-open={isOpenModal}>
+                <AppBar position="sticky">
+                    <Grid>
+                        <Toolbar>
+                            <Typography variant="title" color="inherit">
+                                Weather App
+                            </Typography>
+                        </Toolbar>
+                    </Grid>
+                </AppBar>
+                <Grid>
+                    <WeatherList
+                        cities={citiesID}
+                        onSelectedLocation={this.handleSelectionLocation}
+                    />
+                    {city ? (
+                        <ForeCastExtended
+                            cityID={city.id}
+                            isOpen={isOpenModal}
+                            onHandledModal={this.handledModal}
+                        />
+                    ) : null}
+                </Grid>
+            </div>
+        );
+    }
+}

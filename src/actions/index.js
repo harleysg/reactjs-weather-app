cb38c3ba130e6d0791b481841f0cc66e61bfc7a0
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-07-21 00:04:46 -0500

    udpate: se crea nuevo action toggleModal, TOGGLE_MODAL en actions/index.js. Se declara nuevo caso TOGGLE_MODAL en reducers/city.reducer.js:

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,4 @@
 export const SET_CITY = "SET_CITY";
+export const TOGGLE_MODAL = "TOGGLE_MODAL";
 export const setCity = payload => ({ type: SET_CITY, payload });
+export const toggleModal = payload => ({ type: TOGGLE_MODAL, payload });
--- src/reducers/city.reducer.js
+++ src/reducers/city.reducer.js
@@ -1,9 +1,11 @@
-import { SET_CITY } from "actions";
+import { SET_CITY, TOGGLE_MODAL } from "actions";
 
 export default (state = {}, action) => {
     switch (action.type) {
         case SET_CITY:
             return { ...state, city: action.payload };
+        case TOGGLE_MODAL:
+            return { ...state, isOpenModal: action.payload };
         default:
             return state;
     }

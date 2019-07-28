0889e96ed8799933e99323920a4e83bf7718a8d9
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-07-27 21:01:17 -0500

    update: #reFactor depuración WeatherComponent, se cambia a componente funcional

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,84 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 /** @ui_libraries */
-import { Grid } from "react-flexbox-grid";
-import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
 import PropTypes from "prop-types";
 /** @components */
-import {
-    WeatherListContainer,
-    ForeCastExtendedContainer
-} from "components/containers";
-import ForeCastExtended from "components/Weather/_foreCastExtended";
+import { WeatherListContainer, ForeCastExtendedContainer } from "components/containers";
+import { MUHeaderSticky } from "components/Weather/_header";
+const WeatherComponent = props => {
+    const { cities } = props;
 
-class WeatherComponent extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            city: null,
-            isOpenModal: false
-        };
-    }
-
-    handleSelectionLocation = city => {
-        this.setState({
-            city,
-            isOpenModal: true
-        });
-    };
-
-    handledModal = stateModal => {
-        if (stateModal === false) {
-            this.setState({ isOpenModal: false });
-        } else {
-            this.setState({ isOpenModal: true });
-        }
-    };
-
-    render() {
-        const { city, isOpenModal } = this.state;
-        const { cities } = this.props;
-        return (
-            <div className="c-weather" data-open={isOpenModal}>
-                <AppBar position="sticky">
-                    <Grid>
-                        <Toolbar>
-                            <Typography variant="title" color="inherit">
-                                Weather App
-                            </Typography>
-                        </Toolbar>
-                    </Grid>
-                </AppBar>
-                <Grid>
-                    <WeatherListContainer
-                        cities={cities}
-                        setCity={() => {}}
-                        onSelectedCity={this.handleSelectionLocation}
-                    />
-                    {city ? (
-                        <div>
-                            <ForeCastExtendedContainer
-                                city={city}
-                                isOpen={isOpenModal}
-                                onHandledModal={this.handledModal}
-                            />
-                            {/* <ForeCastExtended
-                                city={city}
-                                isOpen={isOpenModal}
-                                onHandledModal={this.handledModal}
-                            /> */}
-                        </div>
-                    ) : null}
-                </Grid>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="c-weather">
+            <MUHeaderSticky>
+                <Typography variant="title" color="inherit">
+                    Weather App
+                </Typography>
+            </MUHeaderSticky>
+            <main className="o-wrapper">
+                <WeatherListContainer cities={cities} setCity={() => {}} />
+                <ForeCastExtendedContainer />
+            </main>
+        </div>
+    );
+};
 
 WeatherComponent.propTypes = {
-    cities: PropTypes.array.isRequired /**@param array */
+    cities: PropTypes.array.isRequired
 };
 
 export default WeatherComponent;

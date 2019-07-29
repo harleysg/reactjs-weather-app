4b94ede2bb3bf3a66dcaba60721fbfd04fce2596
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-07-28 22:30:07 -0500

    new: arhicvo index.js para los servicios

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export { fetchForecastDataBy, fetchWeatherDataBy } from "services/fetchCityForData";
+export { default as weatherAPI } from "services/getURLWeatherByCity";
+export { default as fetchService } from "services/service.fetch";
+export {
+    default as transformForcastQueryService
+} from "services/service.transformForcastQuery";
+export { default as weatherDataFormat } from "services/service.filterWeatherData";

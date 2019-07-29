6461bb702e3cf5e72a31e8b366e20f0a7ed26c7b
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2019-07-28 22:30:42 -0500

    update: #reFactor, naming de getURLWeatherByCity.js

--- src/services/getURLWeatherByCity.js
+++ src/services/getURLWeatherByCity.js
@@ -1,16 +1,16 @@
 import { url_weather, api_key, url_forecast } from "constants/const.weatherAPI";
 
-const getURLWeatherByCity = cityName => {
-    return `${url_weather}?id=${cityName}&appid=${api_key}`
-}
+const getWeatherBy = cityID => {
+    return `${url_weather}?id=${cityID}&appid=${api_key}`;
+};
 
-const getForecastByCity = cityID => {
-    return `${url_forecast}?id=${cityID}&appid=${api_key}`
-}
+const getForecastBy = cityID => {
+    return `${url_forecast}?id=${cityID}&appid=${api_key}`;
+};
 
 export default {
     get: {
-        weatherCity: getURLWeatherByCity,
-        forcastCity: getForecastByCity,
+        weatherCityBy: getWeatherBy,
+        forcastCityBy: getForecastBy
     }
 };
